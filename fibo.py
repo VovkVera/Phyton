f8b6387e70062827568ad46b6bf8fd62d4f719c4
Vera Vovk <vovkvera@gmail.com>
2019-09-19 10:51:42 +0300

    Update fibo.py

--- fibo.py
+++ fibo.py
@@ -15,3 +15,13 @@ elif n==1:
   print([1])
 else:
   print("please let your number be more then 1")
+
+
+# or
+def fibonacci(n):
+  a = b =1
+  for i in range(n):
+     yield a
+     a, b = b, a + b
+
+print(sum(fibonacci(100)))
